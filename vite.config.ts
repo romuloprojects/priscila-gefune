3a1cd849d4b770745abdf8dfff5df03871e28a18
romuloprojects <romulocassis@gmail.com>
2026-09-15 22:13:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "server",
-    },
-  },
-
   vite: {
     server: {
       allowedHosts: ["priscila-gefune.facilities-ai.com.br"],
